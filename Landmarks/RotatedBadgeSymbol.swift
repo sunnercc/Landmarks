5c48e9fa6284524279169fa9d59bff45f1c4615c
sunner <sunner.cc@outlook.com>
2020-07-26 17:20:50 +0800

    darw paths and shapes

---
+++ Landmarks/RotatedBadgeSymbol.swift
@@ -0,0 +1,26 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by sunner on 2020/7/26.
+//  Copyright © 2020 sunner. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
